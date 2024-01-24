461c72b6de869eeeeccceb7d92658016659be42d
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-01-24 04:39:43 -1200

    feat: add bank package

--- src/main/java/org/example/bankApp/BankAccount.java
+++ src/main/java/org/example/bankApp/BankAccount.java
@@ -1,2 +1,72 @@
-package org.example.bankApp;public class BankAccount {
+package org.example.bankApp;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+    public BankAccount() {
+        this.accountNumber = "";
+        this.accountName = "";
+        this.balance = 0.0;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount){
+        if (amount > this.balance){
+            System.out.println("Insufficient funds");
+        } else {
+            this.balance -= amount;
+            System.out.println("Please take your cash");
+        }
+    }
+
+    public void transfer(double amount, BankAccount account){
+        if (amount > this.balance){
+            System.out.println("Insufficient funds");
+        } else {
+            this.balance -= amount;
+            account.balance += amount;
+            System.out.println("Transfer successful");
+        }
+    }
+
+    public void printDetails(){
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Account Name: " + this.accountName);
+        System.out.println("Balance: " + this.balance);
+    }
 }
--- src/main/java/org/example/bankApp/Main.java
+++ src/main/java/org/example/bankApp/Main.java
@@ -1,2 +1,19 @@
-package org.example.bankApp;public class Main {
+package org.example.bankApp;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount("1234567890", "John Doe", 1000.0);
+        BankAccount account2 = new BankAccount("0987654321", "Jane Doe", 2000.0);
+
+        account1.deposit(500.0);
+        account1.withdraw(200.0);
+        account1.transfer(100.0, account2);
+
+        System.out.println(account1.getAccountName() + " has a balance of " + account1.getBalance());
+
+
+
+
+        System.out.println(account2.getAccountName() + " has a balance of " + account2.getBalance());
+    }
 }
